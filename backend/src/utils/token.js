5c833952065302b003264638040233d6c1b9a0c9
sandeeprepala <153351656+sandeeprepala@users.noreply.github.com>
2025-09-06 13:50:23 +0530

    Delete backend directory

--- backend/src/utils/token.js
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken';
-import { env } from '../config/env.js';
-
-export function signJwt(payload, options = {}) {
-  return jwt.sign(payload, env.JWT_SECRET, { expiresIn: env.JWT_EXPIRES_IN, ...options });
-}
-
-
